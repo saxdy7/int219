40db6a89c03f69059956433c7ffdecd7e21b5f5a
sandeep mamidala <mamidalasandeep5@gmail.com>
2026-04-10 13:52:41 +0530

    Add Tailwind CSS styles and create a profile card layout
    - Created output.css with Tailwind CSS styles including custom properties and utility classes.
    - Added tailwind.html for a profile card layout featuring a user image, name, title, skills, and action buttons.
    - Included a TypeScript file with commented-out code for demonstration purposes.

--- typescript.ts
+++ typescript.ts
@@ -1,2 +1,16 @@
-// let age=475
-// age="hi"
+let user:{name:string, age:number} = {
+    name: "John",
+    age: 30
+}
+
+interface studnet{
+    name:string,
+    age:number
+    isActive:boolean
+}
+
+let s1:studnet = {
+    name: "Alice",
+    age: 22,
+    isActive: true
+}
